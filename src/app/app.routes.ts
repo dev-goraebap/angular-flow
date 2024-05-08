3ac7202cd9665fcb19dc1c456daf3b74e9fc79e7
goraebap <dev.goraebap@gmail.com>
2024-05-08 22:07:21 +0900

    draft: 로그인 페이지 생성 및 라우터 연결

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('../pages').then(m => m.LoginPage)
+    },
+    {
+        path: 'home',
         loadComponent: () => import('../pages').then(m => m.HomePage)
     }
 ];

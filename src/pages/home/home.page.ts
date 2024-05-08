fa1f55a743d3d6dffaabddcc488b3981324a3046
goraebap <dev.goraebap@gmail.com>
2024-05-08 22:03:38 +0900

    draft: home 페이지 생성 및 라우터 연결

---
+++ src/pages/home/home.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'home-page',
+    standalone: true,
+    templateUrl: './home.page.html',
+})
+export class HomePage {
+
+}

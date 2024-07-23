44ecad6e5024528ee47df91fb0f84cd52be282f6
dev.goraebap <dev.goraebap@gmail.com>
2024-07-23 11:29:59 +0900

    Refactor: README 업데이트 및 예제 코드 수정

--- src/features/auth/usecases/refresh-tokens.usecase.ts
+++ src/features/auth/usecases/refresh-tokens.usecase.ts
@@ -10,16 +10,23 @@ export class RefreshTokensUsecase implements RefreshBehavior {
 
     private readonly httpClient = inject(HttpClient);
 
+    // @angular-flow/oauth2 에서 설정된 토큰 스토리지 주입
     private readonly tokenStorage = inject(TOKEN_STORAGE);
 
     refresh(): Observable<TokenResource> {
+        // 토큰 스토리지로부터 리프레시토큰 받아오기
         const { refreshToken } = this.tokenStorage.select();
+
+        // 백엔드와 약속된 방식으로 http 호출
         const header = new HttpHeaders()
-            .set('Authorization', `Bearer ${refreshToken}`)
+            .set('Authorization', `Bearer ${refreshToken}`);
         return this.httpClient.post<TokenResource>(REFRESH_TOKEN_MOCK_URL, {}, {
             headers: header,
+            // ✅ context에 skipOAuth2Flow 컨텍스트를 사용해야합니다.
+            // oauth2Flow 인터셉터를 무시하고 진행하도록 설정합니다.
             context: skipOAuth2Flow
         }).pipe(
+            // 요청 실패 시 후처리
             catchError((res: HttpErrorResponse) => {
                 window.alert(res.error.message);
                 return throwError(() => res);
